56d8fe5fbc3a473c43b9b009659a8187bf6d1d7e
Linkai <linkai861@gmail.com>
2020-10-26 11:20:06 -0400

    v0.0.1alpha
    First alpha release. Completed empty map, all player models, simple movement and mouse look.

---
+++ Among Us/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    [Header("Mouse Lock")] public bool isMouseLocked = true;
+    public float cameraRotateXMin = -90f;
+    public float cameraRotateXMax = 90f;
+
+    [Header("Mouse Sensitivity")] public float mouseSensitivity = 4f;
+    
+
+    private float m_mouseX;
+    private float m_mouseY;
+    private float m_rotateX;
+    private float m_mouseScrollWheel;
+    private Transform m_parent;
+    private Camera m_camera;
+    private float m_fieldOfView;
+
+    private void Awake()
+    {
+        m_parent = transform.parent;
+        m_camera = Camera.main;
+        if (m_camera != null) m_fieldOfView = m_camera.fieldOfView;
+        MouseLock();
+    }
+
+    private void Update()
+    { 
+        MouseInput();
+        RotatePlayY();
+        RotateCameraX();
+    }
+
+    private void MouseInput()
+    {
+        m_mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        m_mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+    }
+
+    private void RotatePlayY()
+    {
+       m_parent.Rotate(Vector3.up * m_mouseX);
+    }
+
+    private void RotateCameraX()
+    {
+        m_rotateX += m_mouseY;
+        m_rotateX = Mathf.Clamp(m_rotateX, cameraRotateXMin, cameraRotateXMax);
+        m_camera.transform.localRotation = Quaternion.Euler(-m_rotateX, 0f, 0f);
+    }
+
+    private void MouseLock()
+    {
+        if (isMouseLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            return;
+        }
+        
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+}
---
+++ Among Us/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 1f;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    public float pushPower = 2.0f;
+
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+
+        if (isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        
+        controller.Move(velocity * Time.deltaTime);
+    }
+    
+    // Interacts with rigidbodiees
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        // no rigidbody
+        if (body == null || body.isKinematic)
+            return;
+
+        // We dont want to push objects below us
+        if (hit.moveDirection.y < -0.3f)
+            return;
+
+        // Calculate push direction from move direction,
+        // we only push objects to the sides never up and down
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+        // If you know how fast your character is trying to move,
+        // then you can also multiply the push velocity by that.
+
+        // Apply the push
+        body.velocity = pushDir * pushPower;
+    }
+
+}
